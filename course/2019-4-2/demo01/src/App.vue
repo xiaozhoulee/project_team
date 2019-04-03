3f73c29c6a739dcb6900ba2ec1299f13cdf416ae
test <test@test.com>
2019-04-03 08:10:37 +0800

    x

---
+++ course/2019-4-2/demo01/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+      <h1>你好 {{message}}</h1>
+      <button @click="changeText">改变message</button>
+      <List></List>
+      <List></List>
+      <List></List>
+      <List></List>
+      <List></List>
+  </div>
+</template>
+
+<script>
+import List from "./components/List"
+export default {
+  data(){
+    return {
+      message:"vue project"
+    }
+  },
+  components:{List},
+  methods:{
+    changeText(){
+      this.message = "html project"
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ course/2019-4-2/demo01/src/components/List.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <ul>
+            <li>菠萝 <span>删除</span></li>
+            <li>苹果 <span>删除</span></li>
+            <li>鸭梨 <span>删除</span></li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
