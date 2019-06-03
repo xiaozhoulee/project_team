68f4c9eae1d2a97355bc1becdef52ff0e5ac87ae
test <test@test.com>
2019-06-03 11:46:17 +0800

    更新axios案例

---
+++ docs/Ajax/frontend/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">班级列表</router-link> |
+      <router-link to="/about">学生列表</router-link>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<style>
+
+</style>
